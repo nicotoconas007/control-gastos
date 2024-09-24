d30b2cb0fba383e4bf76520d2551f3c9ab171ef1
Nicolas Toconas <ntoconas@lexim.solutions>
2024-09-24 20:35:55 -0300

    Agregado componente lista de gastos, funcion helper para formatear la fecha y cierre de modal al usar add-expense

---
+++ src/components/ExpenseDetail.tsx
@@ -0,0 +1,26 @@
+import { formatDate } from "../helpers"
+import { Expense } from "../types"
+import AmountDisplay from "./AmountDisplay"
+
+type ExpenseDetailProps = {
+    expense : Expense
+}
+
+export default function ExpenseDetail({expense} : ExpenseDetailProps) {
+  return (
+    <div className="bg-white shadow-lg p-10 w-full border-gray-200 flex gap-5 items-center">
+        <div>
+
+        </div>
+
+        <div>
+            <p>{ expense.expenseName }</p>
+            <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString()) }</p>
+        </div>
+
+        <AmountDisplay 
+            amount={expense.amount}
+        />
+    </div>
+  )
+}
