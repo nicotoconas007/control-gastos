5798dc01cf61d665e90269e15755a5c857bba227
Nicolas Toconas <ntoconas@lexim.solutions>
2024-09-24 15:07:18 -0300

    Agregado en reducer la funcionalidad para mostrar u ocultar el Modal

--- src/components/ExpenseModal.tsx
+++ src/components/ExpenseModal.tsx
@@ -1,21 +1,26 @@
 import { Fragment } from 'react'
 import { PlusCircleIcon } from '@heroicons/react/24/solid'
 import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react'
+import { useBudget } from '../hooks/useBudget'
+import ExpenseForm from './ExpenseForm'
 
 export default function ExpenseModal() {
+
+  const { state, dispatch } = useBudget()
 
   return (
     <>
       <div className="fixed right-5 bottom-5 flex items-center justify-center">
         <button
           type="button"
+          onClick={() => dispatch({ type: "show-modal"})}
         >
           <PlusCircleIcon className='w-16 h-16 text-blue-600 rounded-full' />
         </button>
       </div>
 
-      <Transition appear show={false} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={() => {}}>
+      <Transition appear show={state.modal} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={() => { dispatch({ type: "close-modal" })}}>
           <TransitionChild
             as={Fragment}
             enter="ease-out duration-300"
@@ -41,7 +46,7 @@ export default function ExpenseModal() {
               >
                 <DialogPanel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
     
-
+                  <ExpenseForm />
     
                 </DialogPanel>
               </TransitionChild>
