9c98b53ef3f6553b162689e9f3b66b396063189d
Nicolas Toconas <ntoconas@lexim.solutions>
2024-09-25 20:12:53 -0300

    Agregada la funcionalidad para filtrar pot categoria

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,20 +1,26 @@
 import { useBudget } from "../hooks/useBudget"
 import { useMemo } from "react"
 import ExpenseDetail from "./ExpenseDetail"
+import { categories } from "../data/categories"
 
 
 export default function ExpenseList() {
 
     const{ state } = useBudget()
-
-    const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses])
+    const filteredExpenses = state.currentCategory ? state.expenses.filter( expense => expense.category === state.currentCategory) : state.expenses // Filtrar expensas por category y luego map
+    const isEmpty = useMemo(() => filteredExpenses.length === 0, [filteredExpenses])
+    const categoryName = useMemo(() => {
+        if (!state.currentCategory) return "Listado de Gastos"; 
+        const category = categories.find((cat) => cat.id === state.currentCategory);
+        return category ? category.name : "Categoría no encontrada";
+      }, [state.currentCategory]);
 
     return (
         <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
             {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No hay gastos</p> : (
                 <>
-                    <p className="text-gray-600 text-2xl font-bold my-5">Listado de Gastos.</p>
-                    {state.expenses.map( expense => (
+                    <p className="text-gray-600 text-2xl font-bold my-5">{categoryName}</p>
+                    {filteredExpenses.map( expense => (
                         <ExpenseDetail 
                             key={expense.id}
                             expense={expense}
--- src/components/FilterByCategory.tsx
+++ src/components/FilterByCategory.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from 'react'
+import { ChangeEvent } from 'react'
 import { categories } from '../data/categories'
 import { useBudget } from '../hooks/useBudget'
 
